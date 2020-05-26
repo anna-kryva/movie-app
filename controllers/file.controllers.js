c924af08d059f669d5e60327910b745015e2191a
Anna Kryva <anna120100@gmail.com>
2020-05-26 05:03:13 +0300

    Update stream reader

--- controllers/file.controllers.js
+++ controllers/file.controllers.js
@@ -6,33 +6,36 @@ const toJsonDocuments = require('../utils/toJsonDocuments');
 module.exports.addMoviesFromFile = (req, res) => {
   const stream = new fs.ReadStream(req.file.path, {encoding: 'UTF-8'});
 
-  stream.on('readable', async () => {
-    let data = stream.read();
-
-    if (data) {
-      if (data[data.length - 1] != '\n') {
-        const lineEndIndex = data.lastIndexOfEnd('\n');
-        data = data.slice(0, lineEndIndex);
-        const extra = data.slice(lineEndIndex + 1);
-        stream.unshift(extra);
-      }
-
-      const documents = toJsonDocuments(data);
-      await Movie.insertMany(documents);
-    }
-  });
-
-  stream.on('end', () => {
-    res.status(200).json({
-      status: 'Movies added successfully',
-    });
-  });
-
   stream.on('error', (error) => {
     if (error.code === 'ENOENT') {
       errorHandler(res, new Error('No such file or directory'));
     } else {
       errorHandler(res, error);
     }
+  });
+
+  let extra = '';
+  stream.on('readable', async () => {
+    let data = stream.read();
+    data = extra + data;
+
+    if (!data) return;
+    if (data.slice(data.length - 2, data.length) != '\n\n') {
+      const lineEndIndex = data.lastIndexOf('\n\n');
+
+      extra = data.slice(lineEndIndex + 1);
+      data = data.slice(0, lineEndIndex+1);
+    } else {
+      extra = '';
+    }
+
+    const documents = toJsonDocuments(data);
+    await Movie.insertMany(documents);
+  });
+
+  stream.on('end', () => {
+    res.status(200).json({
+      status: 'Movies added successfully',
+    });
   });
 };
